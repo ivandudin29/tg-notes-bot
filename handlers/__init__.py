d8c95df84e37b9abbd3084cf3e01ff2300e1a6ef
ivandudin29 <ivan.dudin29@gmail.com>
2026-02-05 10:47:41 +0300

    __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,14 @@
-# Этот файл нужен для корректного импорта пакета
+from aiogram import Router
+
+# Создаем роутеры
+commands_router = Router()
+callbacks_router = Router()
+fsm_handlers_router = Router()
+
+# Импортируем обработчики
+from . import commands
+from . import callbacks
+from . import fsm_handlers
+
+# Экспортируем роутеры для удобства
+__all__ = ['commands_router', 'callbacks_router', 'fsm_handlers_router']
